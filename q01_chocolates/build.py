5be666897eec735555680cc4ad1244fdc12133f5
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-05-21 08:58:44 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -7,6 +7,16 @@ input_list = []
 
 def q01_chocolates(my_list=input_list):
 
+    val = 0
+    final_list = []
 
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            final_list.append(val)
+        else:
+            my_list[i] % 2 != 0
+            val += my_list[i]
+            final_list.append(val)
+    return final_list
 
 
