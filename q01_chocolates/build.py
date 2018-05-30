903427eb52aeb1e74216fa567fff999bacf0da3a
SL345 <sherilyn345@gmail.com>
2018-05-30 11:55:21 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,20 @@
+# %load q01_chocolates/build.py
 
-
-input_list = []
+import numpy as np
 
 # Write your solution here:
 
+def q01_chocolates(my_list):
+    input_list = [0]
+    for num in my_list:
+        if num % 2 == 0 :
+            input_list.append(input_list[-1])
+        else:
+            input_list.append(input_list[-1] + num)
+    input_list.pop(0)
+    return input_list
 
-def q01_chocolates(my_list=input_list):
+
 
 
 
