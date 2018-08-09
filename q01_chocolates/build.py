8b1a8c0c5813db5b7ad6924e027323a2e992ea51
rushabh-mehta <rushabh123@gmail.com>
2018-08-09 09:14:36 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,12 +1,22 @@
+# %load q01_chocolates/build.py
 
-
-input_list = []
 
 # Write your solution here:
 
 
-def q01_chocolates(my_list=input_list):
-
-
+def q01_chocolates(input_list):
+    x=0
+    chocolates_bought=[]
+    for i in range(len(input_list)):
+        if input_list[0]%2!=0:
+            if i%2==0:
+                x+=input_list[i]
+            chocolates_bought.append(x)
+        else:
+            if i%2!=0:
+                x+=input_list[i]
+            chocolates_bought.append(x)
+    return chocolates_bought
+q01_chocolates([3, 4, 5, 8, 9, 10])
 
 
