242a47dd2b7b69bdf96d7b646e0cf3a6c458f2f3
nemkothari <nemkothari@gmail.com>
2018-08-10 22:49:55 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,31 @@
+# %load q01_chocolates/build.py
 
-
-input_list = []
+input_list = [1,2,5,8]
 
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
+    out=[]
+    count = 0
+    for i in my_list:
+       
+        print(count)
+        if count ==0 :
+            if i%2 != 0 :
+                out.append(i)
+            else:
+                out.append(0)
+            count = count +1
+        elif i%2 != 0:
+            out.append(out[count-1] + i)
+            count=count+1
+        elif i%2 == 0:            
+            out.append(out[count-1] )
+            count=count+1
+    return out    
+ 
+
 
 
 
