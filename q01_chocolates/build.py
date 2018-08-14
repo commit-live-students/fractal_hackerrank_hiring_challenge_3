c59940530f375d2f43df444b44c65c20c7c30062
Tusharshah2006 <tusharshah2006@gmail.com>
2018-08-14 19:50:44 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,12 +1,23 @@
-
+# %load q01_chocolates/build.py
 
 input_list = []
 
 # Write your solution here:
 
-
 def q01_chocolates(my_list=input_list):
+    List = []
+    total = 0
+    previous = 0
+    for num in my_list:
+        if(num % 2 != 0):
+            total = total + num
+            previous = total
+            List.append(total)
+        else:
+            List.append(previous)
+    
+    return List
 
-
+q01_chocolates(input_list)
 
 
