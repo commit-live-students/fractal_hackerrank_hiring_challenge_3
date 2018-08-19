a2843ca6a9680ccf4e2cb2faecb1fc6cb50d809a
KamalShillongwala <qamaldas@gmail.com>
2018-08-19 07:26:12 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,22 @@
+# %load q01_chocolates/build.py
 
 
-input_list = []
+input_list = [2, 5, 8, 11, 4]
 
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
+    chocolate_count = 0
+    chocolate_count_list = []
+    for index, i in enumerate(my_list):
+        if my_list[index] % 2 == 0:
+            chocolate_count_list.append(chocolate_count)
+        else:
+            chocolate_count = chocolate_count + my_list[index]
+            chocolate_count_list.append(chocolate_count)
+    return chocolate_count_list
+
 
 
 
