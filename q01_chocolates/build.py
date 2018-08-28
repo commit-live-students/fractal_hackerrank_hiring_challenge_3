52e8a1b000c3069eb7e2b79231c2a9b14def55c5
y0ge5h <c.9.yogesh@gmail.com>
2018-08-28 14:46:50 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,12 +1,15 @@
-
-
+# %load q01_chocolates/build.py
 input_list = []
-
 # Write your solution here:
-
-
-def q01_chocolates(my_list=input_list):
-
-
+def q01_chocolates(days=input_list):
+    sum = 0
+    sum_of_chocolates = []
+    
+    for day in days:
+        if day % 2 != 0:
+            sum = sum + day
+        sum_of_chocolates.append(sum)
+        
+    return sum_of_chocolates
 
 
