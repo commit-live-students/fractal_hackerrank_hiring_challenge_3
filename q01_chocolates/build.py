57daa370024447605f6a1ed9e2476803d7f684bb
sagarpatil232 <sagarpatil232@gmail.com>
2018-08-29 16:23:50 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,3 +1,4 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
@@ -6,6 +7,20 @@ input_list = []
 
 
 def q01_chocolates(my_list=input_list):
+    l1 = []
+    for t in my_list:
+        if (t%2) != 0:
+            if len(l1)>0:
+                l1.append(t+l1[len(l1)-1])
+            else:
+                l1.append(t)
+        else:
+            if len(l1)>0:
+                l1.append(l1[len(l1)-1])
+            else:
+                l1.append(0)
+    return l1
+
 
 
 
