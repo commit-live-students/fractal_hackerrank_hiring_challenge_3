3f084f7c8c730cbe9c69ba7dcc4c446f0c1e812b
avdeshmishra0214 <avdesh.mishra0214@gmail.com>
2018-09-29 09:23:56 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,12 +1,18 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
 
 # Write your solution here:
-
-
 def q01_chocolates(my_list=input_list):
-
-
+    new1 = []
+    iprev= 0 
+    for i in my_list:
+        if i % 2 == 1: 
+            new1.append(i+iprev)
+            iprev = i+iprev
+        else: 
+            new1.append(iprev)
+    return (new1)
 
 
