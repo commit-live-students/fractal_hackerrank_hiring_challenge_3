6ee30319f6f96e6424290d6391a4786a38e2d3a9
sunilhariharan <sunilhariharan@gmail.com>
2018-09-29 09:34:22 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,24 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
+
 
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
+
+    b=[x if x%2==1 else 0 for x in my_list]
+    
+    new_l=[]
+    cumsum=0
+    for i in b:
+        cumsum += i
+        new_l.append(cumsum)
+        
+    return new_l
+
 
 
 
