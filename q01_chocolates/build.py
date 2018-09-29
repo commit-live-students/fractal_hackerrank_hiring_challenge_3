2a22bf0cd3504bf7fa82347bab310de014072ad2
Nile28 <nileshdhongade07@gmail.com>
2018-09-29 10:08:05 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,23 @@
+# %load q01_chocolates/build.py
 
 
-input_list = []
+input_list = [3,4,5,8,9,10,11]
 
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
+    l=0
+    p=[]
+    for x in my_list:
+        if x%2==1:
+            l=l+x
+        p.append(l)
+    return p
+
+
+q01_chocolates(input_list)
+    
 
 
 
