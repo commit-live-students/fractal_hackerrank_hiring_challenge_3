eeb055f472ec98ca09b9ea7909e0a00cf993d19d
preetiail <ailpreeti@ymail.com>
2018-09-29 13:55:39 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,37 @@
+# %load q01_chocolates/build.py
 
 
-input_list = []
+input_list= [2, 5, 8, 11, 4]
+
 
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
+    list2=list()
+    newval=0
+    if len(my_list)%2==0:
+        for index,num in enumerate(my_list):
+            if index%2==0:
+                newval=num+newval
+            else:
+                newval=list2[index-1]
+            list2.append(newval)
+    else:
+            for index,num in enumerate(my_list):
+                if index%2!=0:
+                    newval=num+newval
+                else:
+                    if index==0:
+                        newval=0
+                    else:
+                        newval=list2[index-1]
+                list2.append(newval)
+    return(list2)
+        
+q01_chocolates()
+
+
 
 
 
