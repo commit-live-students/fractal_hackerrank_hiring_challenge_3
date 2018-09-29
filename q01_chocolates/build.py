309ee1b39ee69695841a90c8038b7ea89ffb774a
Abhimanyu22 <link2manyu@hotmail.com>
2018-09-29 09:32:03 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,24 @@
-
-
-input_list = []
-
-# Write your solution here:
-
+input_list = [3, 4, 5, 8, 9, 10]
 
 def q01_chocolates(my_list=input_list):
+    list2 = []
+    values = 0
+    for item in my_list:
+        if item%2 !=0:
+            values +=item
+        
+        list2.append(values)
+    
+    return list2
+
+
+
+
+
+
+
+
+
 
 
 
