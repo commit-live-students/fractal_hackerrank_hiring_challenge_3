a2c90cfa0eb47b55f099789e1920175cdfc9b1ba
prodigy803 <pushkaraj803@gmail.com>
2018-09-29 06:13:34 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,3 +1,4 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
@@ -6,6 +7,31 @@ input_list = []
 
 
 def q01_chocolates(my_list=input_list):
+    #if the pos of list = odd do not consider
+    #if pos is even, get the number, and add it:
+    counter = 0
+    new_list = []
+    if len(my_list)%2==0:
+        for i in range(len(my_list)):
+            if i%2==0:
+                counter +=my_list[i]
+                new_list.append(counter)
+            else:
+                new_list.append(counter)
+        return new_list
+    else:
+        for i in range(len(my_list)):
+            if i%2!=0:
+                counter +=my_list[i]
+                new_list.append(counter)
+            else:
+                new_list.append(counter)
+        return new_list
+
+
+
+
+
 
 
 
