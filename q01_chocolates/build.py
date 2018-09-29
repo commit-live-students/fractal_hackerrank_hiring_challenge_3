077019c9bf9607343faf8a0aa3afa1479dad9bcb
psicktrick <poojarysaurabh932@gmail.com>
2018-09-29 09:23:15 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,3 +1,4 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
@@ -5,7 +6,26 @@ input_list = []
 # Write your solution here:
 
 
-def q01_chocolates(my_list=input_list):
+def q01_chocolates(my_list):
+    l = []
+    for i in my_list:
+    
+        if i% 2 ==1:
+            if not l:
+                l.append(i)
+            else:    
+                l.append(l[-1]+i)
+            
+        elif not l:
+            l.append(0)
+        else:
+            l.append(l[-1])
+            
+    return l
+            
+
+
+
 
 
 
