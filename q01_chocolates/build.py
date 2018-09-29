961edc5affa9552ad5dbdcef826bd017a1ec1164
aksmungekar <mungekarakshay@yahoo.com>
2018-09-29 09:33:10 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,18 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
-
-# Write your solution here:
-
-
 def q01_chocolates(my_list=input_list):
+    list1=[]
+    z=0
+    for i in my_list:
+        if i%2==1:
+            z+=i
+        list1.append(z)
+    return list1
+
+
+
 
 
 
