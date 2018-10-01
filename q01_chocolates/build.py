fb4078131e62d9f5d2cbea7d33faf0eff223be4e
tracedence <shoaibrashid887@gmail.com>
2018-10-01 08:18:18 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -2,21 +2,26 @@
 
 
 #input_list = [1,2,5,8]
-input_list = [3, 4, 5, 8, 9, 10]
+input_list = [1,2,5,8]
 # Write your solution here:
 
 
 def q01_chocolates(my_list=input_list):
-    l = []
-    p =0
+    
+    result_list = [] #empty list for number of element
+    previous_number =0 
+    
     for element in my_list:
-        if element % 2 == 1:
-            p = p + element
-            l.append(p)
-        else:
-            l.append(p)
-    return l
+        if element % 2 == 1: #condition to test element is odd
+            previous_number = previous_number + element
+        
+        result_list.append(previous_number) #append the result
+    
+    return result_list
+
 q01_chocolates()
+l = []
+p = [l.append( element)if element%2 ==1 else l.append(l[len(l)-1]) for element in input_list   ]
 
 
 
