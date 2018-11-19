1749f70cee50f7b84b7325452baf5befd50a4c11
Sumeshks29 <sumeshks29@gmail.com>
2018-11-19 08:18:19 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,3 +1,4 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
@@ -6,6 +7,14 @@ input_list = []
 
 
 def q01_chocolates(my_list=input_list):
+    output_list = []
+    count = 0
+    for i in my_list:
+        if(i%2!=0):
+            count+=i
+        output_list.append(count)
+    return output_list
+
 
 
 
