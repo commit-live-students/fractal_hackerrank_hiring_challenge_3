b64307dfd24350e273ed29308e26c001e97e7c8a
EminentNeo <aniketjagtap18@gmail.com>
2018-11-21 09:45:45 +0000

    Done

--- q01_chocolates/build.py
+++ q01_chocolates/build.py
@@ -1,11 +1,24 @@
+# %load q01_chocolates/build.py
 
 
 input_list = []
 
-# Write your solution here:
+def q01_chocolates(my_list):
+    value = 0 
+    new = []
+    for i in my_list:
+        if i % 2 != 0:
+            value += i
+            new.append(value)
+        if i % 2 == 0:
+            new.append(value)
+    return new
 
 
-def q01_chocolates(my_list=input_list):
+    
+
+
+
 
 
 
